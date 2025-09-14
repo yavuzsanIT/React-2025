a26aa7b8e89f57a665cf87d2e45c50ca303ed028
ARIKBOGA <barikboga42@gmail.com>
2025-09-14 18:23:19 +0300

    Refactor application structure: update index.html title, enhance App component with team members data, and remove unused styles.

--- about-react/src/main.tsx
+++ about-react/src/main.tsx
@@ -1,10 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { createRoot } from "react-dom/client";
+import { StrictMode } from "react";
+import { App } from "./assets/App";
+import "./App.css";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const rootElement: HTMLElement = document.querySelector('#root')!;
+
+createRoot(rootElement).render(
+    <StrictMode>
+        <App />
+    </StrictMode>  
+);
